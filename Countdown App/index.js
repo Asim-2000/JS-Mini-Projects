3e67c9d3923c8b09f06bbf67626c8eb1a00e20f6
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-04-08 02:00:27 +0500

    displayed coundown

--- Countdown App/index.js
+++ Countdown App/index.js
@@ -1,28 +1,38 @@
 // We need the ending date, current date and then we have to subtract in some way to get the time left.
 
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
 const newYear = 'January 1, 2022';
 
-const countDown = () => {
+function countDown(){
     const newYearDate = new Date(newYear);
     const currentDate = new Date();
 
     const Totalseconds = (newYearDate - currentDate) / 1000;
 
-    const days = Math.floor(Totalseconds / 3600 / 24);
-    const hours = Math.floor(Totalseconds / 3600) % 24;
-    const minutes = Math.floor(Totalseconds / 60) % 60;
-    const seconds = Math.floor(Totalseconds)%60
+    const day = Math.floor(Totalseconds / 3600 / 24);
+    const hour = Math.floor(Totalseconds / 3600) % 24;
+    const mins = Math.floor(Totalseconds / 60) % 60;
+    const secs = Math.floor(Totalseconds)%60
 
 
     // const minutes = Math.ceil(seconds / 60);
     // const hours = Math.ceil(minutes / 60);
     // const days = Math.ceil(hours / 24);
 
-    console.log(days,hours,minutes,seconds); // 
+    console.log(day,hour,mins,secs); // 
+
+    daysEl.innerHTML = day;
+    hoursEl.innerHTML = hour;
+    minutesEl.innerHTML = mins;
+    secondsEl.innerHTML = secs;
 
 }
 
 
 countDown();
 
-setInterval(countDown,1000)
+setInterval(countDown, 1000)
