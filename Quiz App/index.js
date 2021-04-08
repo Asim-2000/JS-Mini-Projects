5c48c6ae3a876859995120bbb475fa64dc19ac9b
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-04-09 02:01:11 +0500

    on click functionality

--- Quiz App/index.js
+++ Quiz App/index.js
@@ -41,7 +41,8 @@ const questionEl = document.getElementById('question')
 const a_text=document.getElementById('a_text')
 const b_text=document.getElementById('b_text')
 const c_text=document.getElementById('c_text')
-const d_text=document.getElementById('d_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
 let currentQuestion = 0;
 
@@ -57,6 +58,15 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d;
     
     
-    currentQuestion++;
+    // currentQuestion++;
 
 }
+
+submitBtn.addEventListener("click", () => {
+
+    // if (currentQuestion.correct===)
+
+    currentQuestion++;
+    loadQuiz();
+    
+})
