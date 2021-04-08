02f22e3eb137490ed73e1001fd64cf7ba9e16b00
Muhammad Asim <m.asimkhaskheli@gmail.com>
2021-04-08 23:39:54 +0500

    basic structure and quizlet done

---
+++ Quiz App/index.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "How wold is Asim",
+        a: '10',
+        b: '16',
+        c: '21',
+        d: '55',
+        correct:'c'
+
+    },
+
+    {
+        question: 'What is the most used programming language in 2020',
+        a: 'Java',
+        b: 'Python',
+        c: 'JavScript',
+        d: 'Go',
+        correct: 'a'
+    },
+
+    {
+        question: 'Who is the Prime Minister of Pakistan?',
+        a: 'Asim',
+        b: 'Nawaz',
+        c: 'Zardari',
+        d: 'Imran Khan',
+        correct: 'd'
+    },
+
+    {
+        question: 'Who is the President of Pakistan?',
+        a: 'Asim',
+        b: 'Nawaz',
+        c: 'Zardari',
+        d: 'Arif Ali',
+        correct: 'd'
+    },
+]
